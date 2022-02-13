ccf90f8e61545f729427c809821033525d76b471
codingcity.net <codingcity.net@gmail.com>
2022-02-13 21:45:19 +0900

    500

--- common/views.py
+++ common/views.py
@@ -27,3 +27,8 @@ def page_not_found(request, exception):
     """
     return render(request, 'common/404.html', {})
 
+def internal_server_error(request):
+    """
+    500 internal server error
+    """
+    return render(request, 'common/500.html', {})
